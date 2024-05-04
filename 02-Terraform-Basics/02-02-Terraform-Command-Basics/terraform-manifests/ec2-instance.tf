d8ffd14f7305a7e4b64a3fb7892e26826da705a3
rnalus24 <168394669+rnalus24@users.noreply.github.com>
2024-05-04 06:16:08 -0700

    EC2

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -11,7 +11,7 @@ terraform {
 # Provider Block
 provider "aws" {
   profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
-  region  = "us-east-1"
+  region  = "ap-southeast-1"
 }
 
 # Resource Block
