ca4505566389e249adef561e4ddf445eb2e89809
rnalus24 <168394669+rnalus24@users.noreply.github.com>
2024-05-04 06:17:40 -0700

    AMI

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -16,6 +16,6 @@ provider "aws" {
 
 # Resource Block
 resource "aws_instance" "ec2demo" {
-  ami           = "ami-0be2609ba883822ec" # Amazon Linux in us-east-1, update as per your region
+  ami           = "ami-05b46bc4327cf9d99" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
 }
